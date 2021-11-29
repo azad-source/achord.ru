d4a5ec48fff6b485ea42eaaf6df3b82ab92cc240
dima386 <chinzhin@mail.ru>
2021-11-29 23:47:20 +0400

    Дополнительные поля в запрос /auth/users/me/ для получение прав админа+ тестирование апишки гугла

--- backend/auth_app/urls.py
+++ backend/auth_app/urls.py
@@ -3,7 +3,12 @@ from . import views
 from rest_framework import routers
 
 
-urlpatterns = [
+router = routers.DefaultRouter()
+router.register(r'auth/users', views.UserViewset, basename='users')
+urlpatterns = router.urls
+
+
+urlpatterns += [
     path('auth/jwt/create/google/', views.LoginGoogle.as_view()),
     path('auth/social/links/', views.SocialLinks.as_view()),
 ]
--- backend/auth_app/views.py
+++ backend/auth_app/views.py
@@ -7,14 +7,21 @@ from rest_framework_simplejwt.tokens import RefreshToken
 import requests
 from .oauth import Oauth2Google
 User = get_user_model()
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from . import serializers
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
-def create_jwt_token(user, raw_data=""):
+def create_jwt_token(user, raw_data="", raw_data2=""):
     refresh = RefreshToken.for_user(user)
     return JsonResponse({
         'refresh': str(refresh),
         'access': str(refresh.access_token),
         'raw_data': raw_data,
+        'raw_data2': raw_data2,
     })
 
 
@@ -29,6 +36,7 @@ class LoginGoogle(View):
         Авторизация через одноклассники (Обработка redirect_uri)
     '''
     raw_data = {}
+    raw_data2 = {}
     email = None
     ID = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_ID
     SECRET = settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET
@@ -44,11 +52,12 @@ class LoginGoogle(View):
         if self.email:
             user = User.objects.get_or_create(email=self.email)[0]
             user = self.update_user(user)
-            return create_jwt_token(user, self.raw_data)
+            return create_jwt_token(user, self.raw_data, self.raw_data2)
         else:
             response = JsonResponse({
                 'detail': f'Не удалось получить данные для авторизации',
                 'raw_data': self.raw_data,
+                'raw_data2': self.raw_data2,
             })
             response.status_code = 401
             return response
@@ -60,9 +69,28 @@ class LoginGoogle(View):
         path = f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}"
         credentials = requests.get(path).json()
         self.parse_user_data(credentials)
-        return credentials
-
+        path = f"https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token={access_token}"
+        credentials = requests.get(path).json()
+        self.parse_user_data2(credentials)
+        
     def parse_user_data(self, data):
         self.raw_data = data
         self.email = data.get("email")
 
+    def parse_user_data2(self, data):
+        self.raw_data2 = data
+        
+
+class UserViewset(viewsets.ReadOnlyModelViewSet):
+    serializer_class = serializers.UserSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        instance = get_object_or_404(
+            self.get_queryset(), 
+            id=self.request.user.id
+        )
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
